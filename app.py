b3f835ed1cc59610879722afb9777af27bc7ac97
Simon <hello@wehatemusic.se>
2022-02-25 23:29:49 +0100

    Changed name and added som plans

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from tkinter import *
+# File
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
 
-root = Tk("Hello")
 
-# Creating a Label Widget
-my_label = Label(root, text="Hello World!")
-my_label.pack()
+# Root window
+root = tk.Tk()
+root.title('AutoSlicer')
+
+
+def import_file():
+    filetypes = (("Audio files", "*.wav *.mp3 *.aif"),)
+    # show the open file dialog
+    audio_file = fd.askopenfile(filetypes=filetypes)
+
+
+def export_file():
+    pass
+
+
+# Import/Export buttons
+import_button = ttk.Button(root, text='Import', command=import_file)
+export_button = ttk.Button(root, text='Export', command=export_file)
+# Draw buttons
+import_button.grid(row=2, column=0)
+export_button.grid(row=2, column=1)
+
+
+root.mainloop()
