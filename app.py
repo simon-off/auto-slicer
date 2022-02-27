054440b9543a2c800f8be3fb8d462f9083364de2
Simon <hello@wehatemusic.se>
2022-02-27 05:10:17 +0100

    Late night session! Added lots of stuff. Mostly trying to learn Tkinter

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 # Tkinter
-from importlib import resources
 import tkinter as tk
-from tkinter import ttk
+from tkinter import OFF, ttk
 from tkinter import filedialog as fd
-from matplotlib.pyplot import tight_layout
 # PyDub
 from pydub import AudioSegment
 # MatPlotLib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 plt.style.use("./waveform.mplstyle")
+# Numpy
+import numpy
 
 
 # Root window
 root = tk.Tk()
 root.title('AutoSlicer')
-root.geometry("1080x270")
+root.geometry("1200x300")
+root.configure(bg="#333")
 
 
 # Import and load imput_sample to PyDub
@@ -30,11 +31,16 @@ def import_file():
 def plot_waveform(audio_file):
     # Convert audio to mono and get array och sample data
     mono = audio_file.set_channels(1)
+    mono = mono.set_sample_width(2)
     samples = mono.get_array_of_samples()
+    # Get the seconds for plotting X
+    fps = mono.frame_rate
+    time = numpy.linspace(0, len(samples)/fps, num=len(samples))
     # Plot the waveform with the samples
     fig = plt.figure()
     waveplot = fig.add_subplot(111)
-    waveplot.plot(samples)
+    waveplot.plot(time, samples, color="#FF8800")
+    waveplot.set_yticks([-32768, -16384, 0, 16384, 32767])
     # Draw the plot with Tkinter canvas
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
@@ -45,16 +51,38 @@ def export_file():
     pass
 
 
+
+# Frame for button row
+frame = tk.Frame(root)
+frame.grid(row=1, columnspan=3, sticky="SEW")
+frame.configure(background="#444")
+
+# Import/Export buttons
+import_button = tk.Button(frame, text="Import", command=import_file)
+threshold_input = tk.Spinbox(frame)
+fade_in_input = tk.Spinbox(frame)
+fade_out_input = tk.Spinbox(frame)
+normalize_button = tk.Checkbutton(frame, text="Normalize")
+normalize_button.deselect()
+export_button = tk.Button(frame, text="Export", command=export_file)
+
+# Draw buttons
+buttons = [
+    import_button,
+    threshold_input,
+    fade_in_input,
+    fade_out_input,
+    normalize_button,
+    export_button
+]
+
+for i, button in enumerate(buttons):
+    button.grid(row=1, column=i, sticky="NSEW")
+
 # Configure grid
 tk.Grid.rowconfigure(root, 0, weight=1)
 tk.Grid.columnconfigure(root, 0, weight=1)
-
-# Import/Export buttons
-import_button = ttk.Button(root, text='Import', command=import_file)
-export_button = ttk.Button(root, text='Export', command=export_file)
-# Draw buttons
-import_button.grid(row=1, column=0, sticky="SEW")
-export_button.grid(row=1, column=1, sticky="SEW")
+tk.Grid.columnconfigure(frame, list(range(len(buttons))), weight=1)
 
 
 root.mainloop()
