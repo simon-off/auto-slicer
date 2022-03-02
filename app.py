e3238a163358ec5e32e0b76a5a3a34740f565acb
Simon <hello@wehatemusic.se>
2022-03-02 23:31:52 +0100

    Made lots of progress today! Made the treshold line and exported my first slices!! Now I gotta figure out how to accurately map the slice functions threshold to my janky one.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from tkinter import ttk, font
 from tkinter import filedialog as fd
 from turtle import right
 # PyDub
-from pydub import AudioSegment
+from pydub import AudioSegment, silence
 # MatPlotLib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
@@ -16,24 +16,38 @@ import numpy
 plt.style.use("./waveform.mplstyle")
 
 
+# Define global variables
+filepath = None
+audio_file = None
+threshold = -50
+
+
 # Root window
 root = tk.Tk()
 root.title('AutoSlicer')
 root.geometry("1250x250")
-root.resizable(False, False)
-root.configure(bg="#111")
+root.configure(bg="#191919")
 # Font
 #defaultFont = font.nametofont("TkDefaultFont")
 #defaultFont.configure(family="Roboto Mono", size=10)
 
 
+# Frame to draw graph and lines
+main_frame = tk.Frame(root, bg="#191919")
+main_frame.grid(row=0, sticky="NSEW")
+
+
 # Import and load imput_sample to PyDub
 def import_file():
     filetypes = (("Audio files", "*.wav *.mp3 *.aif *.ogg"),)
+    global filepath
     filepath = fd.askopenfile(filetypes=filetypes).name
     global audio_file
     audio_file = AudioSegment.from_file(filepath)
     plot_waveform()
+    filepath_label.config(text = filepath)
+    audio_length_label.config(text = str(round(audio_file.duration_seconds, 3)) + " SECONDS")
+    draw_lines()
 
 
 # Plot waveform via matplotlib
@@ -48,35 +62,76 @@ def plot_waveform():
     waveplot.set_yticks([-32768, -16384, 0, 16384, 32767])
     
     # Draw the plot with Tkinter canvas
-    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas = FigureCanvasTkAgg(fig, master=main_frame)
     canvas.draw()
     canvas.get_tk_widget().grid(row=0, sticky="NSEW")
+    canvas.get_tk_widget().configure(background='black')
+
+
+line_canvas = tk.Canvas(main_frame)
+# Update and draw Threshold Line
+def draw_lines():
+    line_canvas.configure(bg="#0099FF", height=2, highlightthickness=0)
+    line_canvas.place(relwidth=100, rely=((threshold / -200) - 0.002))
+
+draw_lines()
+
+
+# Command that runs when changing the threshold value
+def update_threshold(input):
+    if input and int(input) <= 0 and int(input) >= -100:
+        global threshold
+        threshold = int(input)
+        draw_lines()
+        return True
+    else:
+        return False
+
+thresh=root.register(update_threshold)
 
 
 # Export Sliced Sample(s)
 def export_file():
-    pass
+    slices = silence.split_on_silence(audio_file, min_silence_len=100)
+    for i, slice in enumerate(slices):
+        with open("test/sound-%s.wav" % i, "wb") as f:
+            slice.export(f, format="wav")
+
+
+# Draw button_frame for label row
+info_frame = tk.Frame(root, bg="#111")
+info_frame.grid(row=1, sticky="EW")
+# Filepath
+filepath_label = tk.Label(info_frame, bg="#111", fg="#666", padx=5)
+filepath_label.grid(sticky="W", row=0, column=0)
+# Audio length
+audio_length_label = tk.Label(info_frame, bg="#111", fg="#666", padx=5)
+audio_length_label.grid(sticky="E", row=0, column=0)
+
 
 # Frame for button row
-time_frame = tk.Frame(root, bg="#666")
-time_frame.grid(row=1, sticky="SEW")
-test_label = tk.Label(time_frame, text="TESTING")
-test_label.grid()
-# Frame for button row
-frame = tk.Frame(root, bg="#444")
-frame.grid(row=2, sticky="SEW")
+button_frame = tk.Frame(root, bg="#222", bd=1)
+button_frame.grid(row=2, sticky="SEW")
 
-# Frame Widgets
-import_button = tk.Button(frame, text="Import", command=import_file)
-threshold_label = tk.Label(frame, text="Threshold ", anchor="e")
-threshold_input = tk.Scale(frame, from_=0, to=-100, orient="horizontal", showvalue=False, highlightthickness=0)
-fade_in_label = tk.Label(frame, text="Fade in ", anchor="e")
-fade_in_input = tk.Spinbox(frame, from_=0, to=1000)
-fade_out_label = tk.Label(frame, text="Fade out ", anchor="e")
-fade_out_input = tk.Spinbox(frame, from_=0, to=1000)
-normalize_button = tk.Checkbutton(frame, text="Normalize")
+
+# Button Widgets
+import_button = tk.Button(button_frame, text="Import", command=import_file)
+
+threshold_label = tk.Label(button_frame, text="Threshold ", anchor="e")
+threshold_input = tk.Spinbox(button_frame, from_=-100, to=0)
+threshold_input.config(validate="all", validatecommand=(thresh, '%P'))
+
+fade_in_label = tk.Label(button_frame, text="Fade in ", anchor="e")
+fade_in_input = tk.Spinbox(button_frame, from_=0, to=1000)
+
+fade_out_label = tk.Label(button_frame, text="Fade out ", anchor="e")
+fade_out_input = tk.Spinbox(button_frame, from_=0, to=1000)
+
+normalize_button = tk.Checkbutton(button_frame, text="Normalize")
 normalize_button.deselect()
-export_button = tk.Button(frame, text="Export", command=export_file)
+
+export_button = tk.Button(button_frame, text="Export", command=export_file)
+
 
 # Draw Frame Widgets
 buttons = [
@@ -93,12 +148,20 @@ buttons = [
 
 for i, button in enumerate(buttons):
     button.grid(row=1, column=i, sticky="NSEW")
-    button.configure(bg="#222", fg="#FFF", activebackground="#333")
+    button.configure(bg="#222", fg="#FFF", activebackground="#333", bd=1)
+
+threshold_input.configure(buttonbackground="#333", insertbackground="white")
+fade_in_input.configure(buttonbackground="#333", insertbackground="white")
+fade_out_input.configure(buttonbackground="#333", insertbackground="white")
+
 
 # Configure grid
 tk.Grid.rowconfigure(root, 0, weight=1)
 tk.Grid.columnconfigure(root, 0, weight=1)
-tk.Grid.columnconfigure(frame, list(range(len(buttons))), weight=1)
+tk.Grid.rowconfigure(main_frame, 0, weight=1)
+tk.Grid.columnconfigure(main_frame, 0, weight=1)
+tk.Grid.columnconfigure(info_frame, 0, weight=1)
+tk.Grid.columnconfigure(button_frame, list(range(len(buttons))), weight=1)
 
 
 root.mainloop()
